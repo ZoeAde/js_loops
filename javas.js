1f21d28bfc718569d60a651584b94607627bab44
Zoe <zadelmanz@gmail.com>
2015-07-10 16:30:49 -0600

    add code for java loop

---
+++ javas.js
@@ -0,0 +1,30 @@
+//number 1:
+for (var i = 0; i <= 1000; i += 100) {
+  console.log(i);
+}
+//number2:
+for (var i = 1; i <= 128; i=i*2) {
+  console.log(i);
+}
+//number3:
+for (var i = 0; i <= 10; i += 2) {
+  console.log(i);
+}
+//number4:
+for (var i = 3; i<= 15; i += 3) {
+  console.log(i);
+}
+//number5:
+for (var i = 9; i >= 0; i--) {
+  console.log(i);
+}
+//number6:
+for (var i = 0; i <=4; i++) {
+  console.log(i + " " + i +  " " + i);
+}
+//number7:
+for  (var t=3; t>=1; t--) {
+for (var i = 0; i <= 4; i++) {
+      console.log(i);
+    }
+}
